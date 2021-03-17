b7c16bcb3b8ec97b8a30ad9864586ecff98f666f
Baradji Amara <baradjiamara17@gmail.com>
2021-03-17 14:56:01 +0000

    r

--- Baradji/settings.py
+++ Baradji/settings.py
@@ -26,44 +26,8 @@ SECRET_KEY = '9hdg(=cnla^6sgfzu=tg0y3$@imae$3*l-^#izkovehc%7t859'
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
-
-    # DATABASES = {
-
-    #     'default': {
-
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-    #         'NAME': 'd484d5j43u4isv',
-
-    #         'USER': 'xyohowygfhivbd',
-
-    #         'PASSWORD': '2b7e27a4dcc5b20271e2d0e08c1ba8b8113c9bf92383f53380c7cd00a625f7d6',
-
-    #         'HOST': 'ec2-34-198-31-223.compute-1.amazonaws.com',
-
-    #         'PORT': '5432',
-
-    #     }
-
-    # }
 else:
     DEBUG = True
-    
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'baradji',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-        }
-}
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,7 +90,21 @@ WSGI_APPLICATION = 'Baradji.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'baradji',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
