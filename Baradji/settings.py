8a7ef7c9c3ed2dfa8a3fc636cedf4a2067cca4e1
Baradji Amara <baradjiamara17@gmail.com>
2021-03-17 00:52:57 +0000

    r

--- Baradji/settings.py
+++ Baradji/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['baradjise-shop.herokuapp.com', '127.0.0.1', 'www.baradjie-shop.ci', '0.0.0.0', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
